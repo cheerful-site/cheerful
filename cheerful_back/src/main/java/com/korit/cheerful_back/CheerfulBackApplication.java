a2333c7798398b1c11f43f98c246bcfc543e87e1
sojeong132 <cleo708028@gmail.com>
2025-08-01 12:39:25 +0900

    first setting

---
+++ cheerful_back/src/main/java/com/korit/cheerful_back/CheerfulBackApplication.java
@@ -0,0 +1,13 @@
+package com.korit.cheerful_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerfulBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheerfulBackApplication.class, args);
+    }
+
+}
---
+++ cheerful_back/src/test/java/com/korit/cheerful_back/CheerfulBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.cheerful_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheerfulBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
