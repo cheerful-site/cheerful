7dbe586405a62f910a53d82781a79e6a21fcd141
JbleMaker <jbojsun@gmail.com>
2025-08-12 12:52:04 +0900

    250812-Front-AdminLoginPage Update

--- cheerful_front/src/pages/Admin/AdminLogin/AdminLogin.jsx
+++ cheerful_front/src/pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,13 +1,19 @@
 /**@jsxImportSource @emotion/react */
 import * as s from "./styles";
+import logo from "../../../../logo/cheerful_login.png";
 
 function AdminLogin(props) {
   return (
     <div css={s.layout}>
       <div css={s.adminLoginContainer}>
+        <img src={logo} alt="" />
+
+        <div css={s.horizon}></div>
+
         <span>ADMIN LOGIN</span>
-        <input type="text" />
-        <input type="password" />
+        <input type="text" placeholder="아이디" />
+        <input type="password" placeholder="비밀번호" />
+        <button>로그인</button>
       </div>
     </div>
   );
