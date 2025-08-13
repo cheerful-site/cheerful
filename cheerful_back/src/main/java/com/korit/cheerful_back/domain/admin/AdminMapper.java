770ce157cdb9af7283f0b6318c75fad65a32f835
sojeong132 <cleo708028@gmail.com>
2025-08-13 12:47:17 +0900

    TokenDto

--- cheerful_back/src/main/java/com/korit/cheerful_back/domain/admin/AdminMapper.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/domain/admin/AdminMapper.java
@@ -5,7 +5,5 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface AdminMapper {
-  int insert(Admin admin);
-  Admin findById(@Param("adminId") Integer adminId);
-  Admin findByProviderId(@Param("providerId") String providerId);
+  Admin findByAdminId(@Param("adminLoginId") Integer adminLoginId);
 }
