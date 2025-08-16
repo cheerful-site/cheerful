52c78c0284e0e98ec99972473c12eee69cb99805
Mi-Kyung331 <uu.3120@gmail.com>
2025-08-16 16:39:11 +0900

    UserController, UserService를 각각 AdminController, AdminService로 옮김

--- cheerful_back/src/main/java/com/korit/cheerful_back/controller/AdminController.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/controller/AdminController.java
@@ -1,7 +1,10 @@
 package com.korit.cheerful_back.controller;
 
+import com.korit.cheerful_back.domain.user.User;
 import com.korit.cheerful_back.dto.admin.AdminLoginReqDto;
+import com.korit.cheerful_back.dto.response.PaginationRespDto;
 import com.korit.cheerful_back.dto.response.ResponseDto;
+import com.korit.cheerful_back.dto.user.UserSearchReqDto;
 import com.korit.cheerful_back.security.model.PrincipalAdmin;
 import com.korit.cheerful_back.service.AdminService;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +12,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
 
     private final AdminService adminService;
+
+    /*
+    관리자 전용 사용자 관리 API
+   */
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AdminLoginReqDto dto) {
@@ -31,5 +40,22 @@ public class AdminController {
     public ResponseEntity<ResponseDto<?>> managerCommunity(@RequestParam Integer page, @RequestParam Integer size
             , @PathVariable Integer categoryId, @RequestParam(required = false) String searchText) {
         return ResponseEntity.ok(ResponseDto.success(adminService.getCommunitySearchList(page, size, categoryId, searchText)));
+    }
+
+    /*
+        사용자 조회
+     */
+    @GetMapping("/manager/users")
+    public ResponseEntity<ResponseDto<PaginationRespDto<User>>> searchUsers(UserSearchReqDto dto) {
+        return ResponseEntity.ok(ResponseDto.success(adminService.searchUsers(dto)));
+    }
+
+    /*
+      사용자 삭제
+     */
+    @DeleteMapping("/manager/users")
+    public ResponseEntity<ResponseDto<?>> deleteUserIds(@RequestParam List<Integer> userIds) {
+        adminService.delete(userIds);
+        return ResponseEntity.ok(ResponseDto.success("회원 정보를 삭제하였습니다."));
     }
 }
--- cheerful_back/src/main/java/com/korit/cheerful_back/service/UserService.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/service/UserService.java
@@ -1,47 +1,47 @@
-package com.korit.cheerful_back.service;
-
-import com.korit.cheerful_back.domain.user.User;
-import com.korit.cheerful_back.domain.user.UserMapper;
-import java.util.List;
-
-import com.korit.cheerful_back.dto.response.PaginationRespDto;
-import com.korit.cheerful_back.dto.user.UserSearchReqDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-/*
-  사용자 관리 서비스 (관리자용)
- */
-@Service
-@RequiredArgsConstructor
-public class UserService {
-  private final UserMapper userMapper;
-
-  /*
-    사용자 검색 결과를 페이징 처리해서 반환
-   */
-  public PaginationRespDto<User> searchUsers(UserSearchReqDto dto) {
-    Integer totalElements = userMapper.getCountOfOptions(dto.toOption());
-    Integer totalPages = (int) Math.ceil(totalElements.doubleValue() / dto.getSize().doubleValue());
-    List<User> foundUsers = userMapper.findAllBySearchOption(dto.toOption());
-    boolean isLast = dto.getPage().equals(totalPages);
-
-    return PaginationRespDto.<User>builder()
-            .content(foundUsers)
-            .totalElements(totalElements)
-            .totalPages(totalPages)
-            .isLast(isLast)
-            .page(dto.getPage())
-            .size(dto.getSize())
-            .build();
-  }
-
-  /*
-    전달된 사용자 id 목록을 모두 삭제
-   */
-  @Transactional(rollbackFor = Exception.class)
-  public void delete(List<Integer> userIds) {
-    userMapper.deleteByUserIds(userIds);
-  }
-}
+//package com.korit.cheerful_back.service;
+//
+//import com.korit.cheerful_back.domain.user.User;
+//import com.korit.cheerful_back.domain.user.UserMapper;
+//import java.util.List;
+//
+//import com.korit.cheerful_back.dto.response.PaginationRespDto;
+//import com.korit.cheerful_back.dto.user.UserSearchReqDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+///*
+//  사용자 관리 서비스 (관리자용)
+// */
+//@Service
+//@RequiredArgsConstructor
+//public class UserService {
+//  private final UserMapper userMapper;
+//
+//  /*
+//    사용자 검색 결과를 페이징 처리해서 반환
+//   */
+//  public PaginationRespDto<User> searchUsers(UserSearchReqDto dto) {
+//    Integer totalElements = userMapper.getCountOfOptions(dto.toOption());
+//    Integer totalPages = (int) Math.ceil(totalElements.doubleValue() / dto.getSize().doubleValue());
+//    List<User> foundUsers = userMapper.findAllBySearchOption(dto.toOption());
+//    boolean isLast = dto.getPage().equals(totalPages);
+//
+//    return PaginationRespDto.<User>builder()
+//            .content(foundUsers)
+//            .totalElements(totalElements)
+//            .totalPages(totalPages)
+//            .isLast(isLast)
+//            .page(dto.getPage())
+//            .size(dto.getSize())
+//            .build();
+//  }
+//
+//  /*
+//    전달된 사용자 id 목록을 모두 삭제
+//   */
+//  @Transactional(rollbackFor = Exception.class)
+//  public void delete(List<Integer> userIds) {
+//    userMapper.deleteByUserIds(userIds);
+//  }
+//}
