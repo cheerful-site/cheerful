7b133e4fbf4583d1d3df94f95c6abc8c52eb0705
sojeong132 <cleo708028@gmail.com>
2025-08-16 16:13:13 +0900

    adminCommunity 페이지 띄우기

--- cheerful_back/src/main/java/com/korit/cheerful_back/service/AdminService.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/service/AdminService.java
@@ -2,15 +2,21 @@ package com.korit.cheerful_back.service;
 
 import com.korit.cheerful_back.domain.admin.Admin;
 import com.korit.cheerful_back.domain.admin.AdminMapper;
+import com.korit.cheerful_back.domain.community.Community;
+import com.korit.cheerful_back.domain.community.CommunityMapper;
+import com.korit.cheerful_back.domain.community.CommunitySearchOption;
 import com.korit.cheerful_back.domain.user.User;
 import com.korit.cheerful_back.domain.user.UserMapper;
 import com.korit.cheerful_back.dto.admin.AdminLoginReqDto;
 import com.korit.cheerful_back.dto.admin.TokenDto;
+import com.korit.cheerful_back.dto.response.PaginationRespDto;
 import com.korit.cheerful_back.exception.auth.LoginException;
 import com.korit.cheerful_back.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +25,7 @@ public class AdminService {
     private final BCryptPasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
     private final AdminMapper adminMapper;
+    private final CommunityMapper communityMapper;
 
     public TokenDto login(AdminLoginReqDto dto) {
 
@@ -31,6 +38,36 @@ public class AdminService {
         }
         return TokenDto.builder()
                 .accessToken(jwtUtil.generateAdminAccessToken(foundAdmin))
+                .build();
+    }
+
+
+    /*
+        admin 전용 커뮤니티 페이징 목록 조회
+     */
+    public PaginationRespDto<Community> getCommunitySearchList(Integer page, Integer size, Integer categoryId, String searchText) {
+        CommunitySearchOption searchOption = CommunitySearchOption.builder()
+                .startIndex((page - 1) * size)
+                .endIndex(size * page)
+                .size(size)
+                .categoryId(categoryId)
+                .searchText(searchText)
+                .build();
+
+        // 총 건수 / 총 페이지 / 마지막 여부 계산
+        List<Community> contents = communityMapper.findAllBySearchOption(searchOption);
+        Integer totalElements = communityMapper.getCountOfSearchOption(searchOption);
+        Integer totalPages = (int) Math.ceil(totalElements.longValue() / size.doubleValue());
+        Boolean isLast = page >= totalPages;
+
+        return PaginationRespDto.<Community>builder()
+                .content(contents)
+                .categoryId(categoryId)
+                .totalElements(totalElements)
+                .totalPages(totalPages)
+                .isLast(isLast)
+                .page(page)
+                .size(size)
                 .build();
     }
 }
