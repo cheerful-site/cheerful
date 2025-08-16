4e8a015a11b2fea2c9c55d777c50005a13b7d9bd
Mi-Kyung331 <uu.3120@gmail.com>
2025-08-16 16:15:25 +0900

    admin users page

---
+++ cheerful_back/src/main/java/com/korit/cheerful_back/dto/user/UserSearchReqDto.java
@@ -0,0 +1,19 @@
+package com.korit.cheerful_back.dto.user;
+
+import com.korit.cheerful_back.domain.user.UserSearchOption;
+import lombok.Data;
+
+@Data
+public class UserSearchReqDto {
+    private Integer page;
+    private Integer size;
+    private String searchText;
+
+    public UserSearchOption toOption() {
+        return UserSearchOption.builder()
+                .startIndex((page - 1) * size)
+                .size(size)
+                .searchText(searchText)
+                .build();
+    }
+}
