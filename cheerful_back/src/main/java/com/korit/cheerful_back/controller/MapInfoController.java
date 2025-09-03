5ac0b0e3792d047cbe8e7607ee6a4eabf6b82fff
Mi-Kyung331 <uu.3120@gmail.com>
2025-09-03 16:10:15 +0900

    map data 위도경도 객체 형태로 전달

--- cheerful_back/src/main/java/com/korit/cheerful_back/controller/MapInfoController.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/controller/MapInfoController.java
@@ -2,6 +2,7 @@ package com.korit.cheerful_back.controller;
 
 
 import com.korit.cheerful_back.domain.map.MapInfo;
+import com.korit.cheerful_back.dto.map.MapInfoRespDto;
 import com.korit.cheerful_back.dto.map.MapSearchReqDto;
 import com.korit.cheerful_back.dto.response.ResponseDto;
 import com.korit.cheerful_back.service.MapInfoService;
@@ -18,25 +19,30 @@ public class MapInfoController {
 
     private final MapInfoService mapInfoService;
 
-//    @GetMapping("/search")
-//    public ResponseEntity<ResponseDto<?>> search(
-//            @RequestParam double lat,
-//            @RequestParam double lng,
-//            @RequestParam(defaultValue = "3000") int radius,
-//            @RequestParam int categoryId
-//    ) {
-//        var req = new MapSearchReqDto(lat, lng, radius, categoryId);
-//        var result = mapInfoService.search(req);
-//        System.out.println(categoryId);
-//        return ResponseEntity.ok(ResponseDto.success(result));
-//    }
+    @GetMapping("/search")
+    public ResponseEntity<ResponseDto<?>> search(
+            @RequestParam double lat,
+            @RequestParam double lng,
+            @RequestParam(defaultValue = "3000") int radius,
+            @RequestParam int categoryId
+    ) {
+        var req = new MapSearchReqDto(lat, lng, radius, categoryId);
+        var result = mapInfoService.search(req);
+        System.out.println(categoryId);
+        return ResponseEntity.ok(ResponseDto.success(result));
+    }
 
 
     @GetMapping("/{categoryId}")
-    public ResponseEntity<ResponseDto<?>> getMapInfo(@PathVariable Integer categoryId, @RequestParam double lat,
+    public ResponseEntity<ResponseDto<?>> getMapInfo(@PathVariable Integer categoryId,
+                                                     @RequestParam double lat,
                                                      @RequestParam double lng,
-                                                     @RequestParam(defaultValue = "5000") int radius) {
+                                                     @RequestParam(defaultValue = "3000") int radius) {
         List<MapInfo> mapInfoList = mapInfoService.mapInfoList(categoryId, lat, lng, radius);
-        return ResponseEntity.ok(ResponseDto.success(mapInfoList));
+
+        // DTO 변환
+        List<MapInfoRespDto> dtoList = MapInfoRespDto.map(mapInfoList);
+
+        return ResponseEntity.ok(ResponseDto.success(dtoList));
     }
 }
--- cheerful_back/src/main/java/com/korit/cheerful_back/domain/map/MapInfo.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/domain/map/MapInfo.java
@@ -13,12 +13,15 @@ public class MapInfo {
   private String mapInfoName;
   private Integer mapInfoCategoryId;
   private String mapInfoAddress;
-  private String mapInfoOperationTime;
   private String mapInfoPhoneNumber;
   private Double mapInfoLat;
   private Double mapInfoLng;
+  private String mapInfoOperationTime;
+  private String mapInfoBreakTime;
   private Boolean mapInfoFullTime;
+  private Boolean mapInfoSpecialAnimal;
   private String mapInfoContent;
 
   private MapInfoCategory mapInfoCategory;
+
 }
