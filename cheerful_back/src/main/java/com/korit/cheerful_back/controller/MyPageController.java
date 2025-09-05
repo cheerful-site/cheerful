e0d4c67587e1b9d586e3ea5d55b47cdde73f26c8
sojeong132 <cleo708028@gmail.com>
2025-09-05 17:42:05 +0900

    fds

--- cheerful_back/src/main/java/com/korit/cheerful_back/controller/MyPageController.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/controller/MyPageController.java
@@ -6,13 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -53,21 +46,5 @@ public class MyPageController {
     return ResponseEntity.ok(ResponseDto.success("프로필 이미지가 변경되었습니다."));
   }
 
-  /*
-    특정 food 글에 좋아요 추가
-  */
-  @PostMapping("/{foodId}/like")
-  public ResponseEntity<ResponseDto<?>> getLike(@PathVariable Integer foodId) {
-    myPageService.like(foodId);
-    return ResponseEntity.ok(ResponseDto.success("좋아요"));
-  }
 
-  /*
-    특정 food 글을 좋아요 취소
-   */
-  @DeleteMapping("/{foodId}/dislike")
-  public ResponseEntity<ResponseDto<?>> getDisLike(@PathVariable Integer foodId) {
-    myPageService.disLike(foodId);
-    return ResponseEntity.ok(ResponseDto.success("좋아요 취소"));
-  }
 }
