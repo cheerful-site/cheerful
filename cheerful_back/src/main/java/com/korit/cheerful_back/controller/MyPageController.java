d1ee0e20aa9080a4a4892e1e65fbbc0c9f662a87
sojeong132 <cleo708028@gmail.com>
2025-09-05 17:40:28 +0900

    f

--- cheerful_back/src/main/java/com/korit/cheerful_back/controller/MyPageController.java
+++ cheerful_back/src/main/java/com/korit/cheerful_back/controller/MyPageController.java
@@ -4,6 +4,8 @@ import com.korit.cheerful_back.dto.response.ResponseDto;
 import com.korit.cheerful_back.service.MyPageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +33,24 @@ public class MyPageController {
   @GetMapping("/food")
   public ResponseEntity<ResponseDto<?>> getMyPageFoodList(@RequestParam Integer page, @RequestParam Integer size) {
     return ResponseEntity.ok(ResponseDto.success(myPageService.getMyPageFoodList(page, size)));
+  }
+
+  @DeleteMapping("/user")
+  public ResponseEntity<ResponseDto<?>> deleteMemberShip() {
+    myPageService.deleteUser();
+    return ResponseEntity.ok(ResponseDto.success("회원 탈퇴 및 작성한 글/댓글이 모두 삭제되었습니다."));
+  }
+
+  @PutMapping("/user/name")
+  public ResponseEntity<ResponseDto<?>> modifyProfileName(@RequestParam String name) {
+    myPageService.modifyProfileName(name);
+    return ResponseEntity.ok(ResponseDto.success("프로필 닉네임이 변경되었습니다."));
+  }
+
+  @PutMapping("/user/image")
+  public ResponseEntity<ResponseDto<?>> modifyProfileImage(@ModelAttribute MultipartFile file) {
+    myPageService.modifyProfileImg(file);
+    return ResponseEntity.ok(ResponseDto.success("프로필 이미지가 변경되었습니다."));
   }
 
   /*
